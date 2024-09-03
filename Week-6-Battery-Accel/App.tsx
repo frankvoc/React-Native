ed1fa6d0b746b8f5e5b13f4de71d5e6a35ae26fd
frankvoc <fmvocatura@email.neit.edu>
2024-09-03 12:38:12 -0400

    Expo Accelerometer
    Added Accelerometer to the existing Battery app and displayed both on the same screen.

--- Week-6-Battery-Accel/App.tsx
+++ Week-6-Battery-Accel/App.tsx
@@ -1,12 +1,22 @@
 import React, {useEffect, useState} from "react";
 import {View, Text, StyleSheet} from 'react-native';
 import * as Battery from 'expo-battery';
+import { Accelerometer } from "expo-sensors";
 
-const App = () => {
+const BatteryAndAccel = () => {
+  //Battery state
   const [batteryLevel, setBatteryLevel] = useState<number | null>(null);
   const [isCharging, setIsCharging] = useState<boolean | null>(null);
 
+  //Accel State
+  const [accelerometerData, setAccelerometerData] = useState<{ x: number; y: number; z: number }>({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+
   useEffect(()=> {
+    //BatteryInfo
     const getBatteryInformation = async () => {
       const level = await Battery.getBatteryLevelAsync();
       const charging = await Battery.getBatteryStateAsync();
@@ -14,27 +24,41 @@ const App = () => {
       setIsCharging(charging === Battery.BatteryState.CHARGING);
     };
     getBatteryInformation();
-
+    //Subscribe to battery and state changes
     const batteryLevelSub =  Battery.addBatteryLevelListener(({ batteryLevel })=>{
       setBatteryLevel(batteryLevel);
     });
     const batteryStateSub = Battery.addBatteryStateListener(({batteryState})=>{
       setIsCharging(batteryState === Battery.BatteryState.CHARGING);
     });
+    //accel sub
+    const accelerometerSub = Accelerometer.addListener((data)=>{
+      setAccelerometerData(data);
+    });
+
+    //accel update interval(1s)
+    Accelerometer.setUpdateInterval(100);
     return () =>{
       batteryLevelSub.remove();
       batteryStateSub.remove();
+      accelerometerSub && accelerometerSub.remove();
     };
   },[]);
 
   return (
     <View style={styles.container}>
+      {/* Start of Battery */}
       <Text style={styles.text}>
         BatteryLevel: {batteryLevel !== null ? `${(batteryLevel * 100).toFixed(0)}%` : 'Loading...'}
       </Text>
       <Text style={styles.text}>
         Charging: {isCharging !== null ? (isCharging ? 'Yes' : 'No'): 'Loadiing'}
       </Text>
+      {/* Start of Accel */}
+      <Text style={styles.text}>Accelerometer</Text>
+      <Text style={styles.text}>x: {accelerometerData.x.toFixed(2)}</Text>
+      <Text style={styles.text}>y: {accelerometerData.y.toFixed(2)}</Text>
+      <Text style={styles.text}>z: {accelerometerData.z.toFixed(2)}</Text>
     </View>
   );
 };
@@ -50,4 +74,4 @@ const styles = StyleSheet.create({
     marginVertical: 5,
   },
 })
-export default App;
+export default BatteryAndAccel;
